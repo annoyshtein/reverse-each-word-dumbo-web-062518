d7bb44afe56713111db57c695caee9ac5acefb6f
Alex Neustein <alex.neustein@gmail.com>
2018-06-21 16:25:51 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,14 +1,9 @@
 def reverse_each_word(string)
-  puts string
   converted_array = string.split(" ")
-  puts "Working on #{converted_array.inspect}"
   converted_array.each do |word|
-    puts "Working on #{word}"
     word.reverse!
-    puts "Worked on #{word}"
   end
-  puts "Worked on #{converted_array.inspect}"
-  puts converted_array.join(" ")
+  converted_array.join(" ")
 end
 
 reverse_each_word("Hello, how are you?")
