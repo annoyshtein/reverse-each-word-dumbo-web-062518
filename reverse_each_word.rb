e428519dd615880fe2447187822e46f99e5f8367
Alex Neustein <alex.neustein@gmail.com>
2018-06-21 16:19:47 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,9 @@ def reverse_each_word(string)
   puts string
   converted_array = string.split(" ")
   puts converted_array.inspect
-  
+  converted_array.each do |word|
+    puts "Taking out #{apple}"
+  end
 end
 
 reverse_each_word("Hello, how are you?")
