c368da41cd5f309ecc28ad773db8a5ac191b686e
Alex Neustein <alex.neustein@gmail.com>
2018-06-21 16:26:39 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(string)
   converted_array = string.split(" ")
   converted_array.collect do |word|
-    word.reverse!
+    word.reverse
   end
   converted_array.join(" ")
 end
